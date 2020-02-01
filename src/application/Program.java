ab1afd4e4589cabbf98c3784b7209b3b580cddfb
Alexandre C. Barbosa <alexandre.cbar@gmail.com>
2020-02-01 16:50:34 -0300

    Little improvement in board printing

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,14 @@
 package application;
 
-import boardgame.Board;
-import boardgame.Piece;
 import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-			
 		ChessMatch chessMatch = new ChessMatch();
 		UI.printBoard(chessMatch.getPieces());
-		
-	}	
-	
+
+	}
+
 }
