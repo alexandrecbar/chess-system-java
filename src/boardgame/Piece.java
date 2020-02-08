be6cc8ee4c9a250219a471ba5dc8e215882e65f0
Alexandre C. Barbosa <alexandre.cbar@gmail.com>
2020-02-08 16:50:25 -0300

    Possible moves of a piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -14,14 +14,6 @@ public abstract class Piece {
 		return board;
 	}
 
-	public abstract boolean[][] possibleMoves();
-
-	public boolean posibleMovie(Position position) {
-
-		return possibleMoves()[position.getRow()][position.getColumn()];
-
-	}
-
 	public boolean isThereAnyPossibleMove() {
 		boolean[][] mat = possibleMoves();
 		for (int i = 0; i < mat.length; i++) {
@@ -32,5 +24,13 @@ public abstract class Piece {
 			}
 		}
 		return false;
+	}
+	
+	public abstract boolean[][] possibleMoves();
+
+	public boolean posibleMovie(Position position) {
+
+		return possibleMoves()[position.getRow()][position.getColumn()];
+
 	}
 }
--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -3,7 +3,7 @@ package chess;
 import boardgame.Board;
 import boardgame.Piece;
 
-public class ChessPiece extends Piece{
+public abstract class ChessPiece extends Piece{
 	
 	private Color color;
 
@@ -14,6 +14,12 @@ public class ChessPiece extends Piece{
 
 	public Color getColor() {
 		return color;
+	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 	
