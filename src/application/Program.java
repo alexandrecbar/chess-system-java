5ae3533e75c93dbccad2cfa663479aa2dda47afc
Alexandre C. Barbosa <alexandre.cbar@gmail.com>
2020-02-09 16:30:43 -0300

    Switching player each turn

--- src/application/Program.java
+++ src/application/Program.java
@@ -19,7 +19,8 @@ public class Program {
 		while (true) {
 
 			try {
-				UI.printBoard(chessMatch.getPieces());
+				UI.clearScreen();
+				UI.printMatch(chessMatch);
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChesPosition(sc);
